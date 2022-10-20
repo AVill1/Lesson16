052a656956697092cae1b882609b422da64b8837
Avill1 <anastasiya6379753@gmail.com>
2022-10-20 21:02:50 +0300

    new commit

---
+++ task03.py
@@ -0,0 +1,12 @@
+import string
+
+print (string.digits)
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(type(string.ascii_letters))
+print(string.hexdigits)
+print(string.octdigits)
+print(string.printable)
+print(string.punctuation)
+print(string.whitespace)
