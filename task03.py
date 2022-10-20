bbec2e9c115638671d4ae3f903e32f51b8a9697f
Avill1 <anastasiya6379753@gmail.com>
2022-10-20 21:29:01 +0300

    new commit

--- task03.py
+++ task03.py
@@ -1,12 +1,28 @@
+
+
+ls = [i *10 for i in range(10)]
+print(ls)
+
+ls = [i *10 for i in range(10)]
+print(ls)
+
+s="asd"
+STR="qwe"
+ls = [0,0]
+print(ls)
+
+print(all(ls))
+print(any(ls))
+
 import string
 
-print (string.digits)
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(type(string.ascii_letters))
-print(string.hexdigits)
-print(string.octdigits)
-print(string.printable)
-print(string.punctuation)
-print(string.whitespace)
+#print (string.digits)
+#print(string.ascii_letters)
+#print(string.ascii_lowercase)
+#print(string.ascii_uppercase)
+#print(type(string.ascii_letters))
+#print(string.hexdigits)
+#print(string.octdigits)
+#print(string.printable)
+#print(string.punctuation)
+#print(string.whitespace)
